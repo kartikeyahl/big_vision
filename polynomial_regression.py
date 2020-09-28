a6a6e55a2b2cd756d7d6174144f546a5c6078ab5
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-09-28 12:11:15 +0530

    Update polynomial_regression.py

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -6,14 +6,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('Position_Salaries.csv')             #endpoint 1(input of .csv file)
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
-
-# Training the Linear Regression model on the whole dataset
-from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression()
-lin_reg.fit(X, y)
+l=len(X[1,:])
 
 # Training the Polynomial Regression model on the whole dataset
 from sklearn.preprocessing import PolynomialFeatures
@@ -22,34 +18,12 @@ X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
-# Visualising the Linear Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Linear Regression)')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
-plt.show()
-
-# Visualising the Polynomial Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
-
-# Visualising the Polynomial Regression results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.1)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
-
-# Predicting a new result with Linear Regression
-lin_reg.predict([[6.5]])
+#taking user input for prediction
+lst=[]
+for i in range(0, l):                           
+    ele = float(input())                          #endpoint 2(taking user input values)
+    lst.append(ele) 
 
 # Predicting a new result with Polynomial Regression
-lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
+y_pred=lin_reg_2.predict(poly_reg.fit_transform([lst]))
+print(y_pred)                                    #endpoint 3(output)
