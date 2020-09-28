2335140e2aef176ad9f6130b5df7b6b98a6b8d8c
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-09-28 12:20:23 +0530

    Update support_vector_regression.py

--- support_vector_regression.py
+++ support_vector_regression.py
@@ -6,45 +6,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('Position_Salaries.csv')                  #endpoint 1(input of .csv file)
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
-print(X)
-print(y)
-y = y.reshape(len(y),1)
-print(y)
-
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-sc_y = StandardScaler()
-X = sc_X.fit_transform(X)
-y = sc_y.fit_transform(y)
-print(X)
-print(y)
+l=len(X[1,:])
 
 # Training the SVR model on the whole dataset
 from sklearn.svm import SVR
 regressor = SVR(kernel = 'rbf')
 regressor.fit(X, y)
 
+#taking user input for prediction
+lst=[]
+for i in range(0, l):                           
+    ele = float(input())                          #endpoint 2(taking user input values)
+    lst.append(ele)   
+
 # Predicting a new result
-sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])))
-
-# Visualising the SVR results
-plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
-plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X)), color = 'blue')
-plt.title('Truth or Bluff (SVR)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
-
-# Visualising the SVR results (for higher resolution and smoother curve)
-X_grid = np.arange(min(sc_X.inverse_transform(X)), max(sc_X.inverse_transform(X)), 0.1)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
-plt.plot(X_grid, sc_y.inverse_transform(regressor.predict(sc_X.transform(X_grid))), color = 'blue')
-plt.title('Truth or Bluff (SVR)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
+y_pred=sc_y.inverse_transform(regressor.predict(sc_X.transform([lst])))
+print(y_pred)                                      #endpoint 3(output)
