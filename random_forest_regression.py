9cafc4e3f8bcf8b6e1cb47cb482df276345e84eb
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-09-28 12:14:19 +0530

    Update random_forest_regression.py

--- random_forest_regression.py
+++ random_forest_regression.py
@@ -6,24 +6,23 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('Position_Salaries.csv')          #endpoint 1(input of .csv file)
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
+l=len(X[1,:])
 
 # Training the Random Forest Regression model on the whole dataset
 from sklearn.ensemble import RandomForestRegressor
 regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
 regressor.fit(X, y)
 
-# Predicting a new result
-regressor.predict([[6.5]])
+#taking user input for prediction
+lst=[]
+for i in range(0, l):                           
+    ele = float(input())                          #endpoint 2(taking user input values)
+    lst.append(ele)               
 
-# Visualising the Random Forest Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Random Forest Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
+# Predicting a new result
+y_pred=regressor.predict([lst])                         
+print(y_pred)                                      #endpoint 3(output)
+
