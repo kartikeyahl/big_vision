9b6082ecc1857b3a38e35e25b206f4b599b7988f
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-10-01 21:45:23 +0530

    Update artificial_neural_network.py

--- artificial_neural_network.py
+++ artificial_neural_network.py
@@ -9,34 +9,21 @@ tf.__version__
 # Part 1 - Data Preprocessing
 
 # Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+dataset = pd.read_csv('Churn_Modelling.csv')               #endpoint 1(input of .csv file)
 X = dataset.iloc[:, 3:-1].values
 y = dataset.iloc[:, -1].values
-print(X)
-print(y)
-
-# Encoding categorical data
-# Label Encoding the "Gender" column
-from sklearn.preprocessing import LabelEncoder
-le = LabelEncoder()
-X[:, 2] = le.fit_transform(X[:, 2])
-print(X)
-# One Hot Encoding the "Geography" column
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
-print(X)
-
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X = sc.fit_transform(X)
-print(X)
+l=len(X[1,:])
+p=(l+1)/2
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
 # Part 2 - Building the ANN
 
@@ -44,10 +31,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 ann = tf.keras.models.Sequential()
 
 # Adding the input layer and the first hidden layer
-ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=p, activation='relu'))
 
 # Adding the second hidden layer
-ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=p, activation='relu'))
 
 # Adding the output layer
 ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
@@ -62,12 +49,12 @@ ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
 
 # Part 4 - Making the predictions and evaluating the model
 
+#taking user input for prediction
+lst=[]
+for i in range(0, l):                           
+    ele = float(input())                          #endpoint 2(taking user input values)
+    lst.append(ele)  
+    
 # Predicting the Test set results
-y_pred = ann.predict(X_test)
-y_pred = (y_pred > 0.5)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
+y_pred=ann.predict(sc.transform([lst]))
+print(y_pred)                                      #endpoint 3(output)
