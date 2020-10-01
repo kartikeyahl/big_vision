ff94e233744326dc0f0ec374536be2c222b12242
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-10-01 21:52:33 +0530

    Update convolutional_neural_network.py

--- convolutional_neural_network.py
+++ convolutional_neural_network.py
@@ -1,11 +1,11 @@
-# Convolutional Neural Network
+# using Convolutional Neural Network
 
 # Importing the libraries
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 tf.__version__
 
-# Part 1 - Data Preprocessing
+# Data Preprocessing
 
 # Generating images for the Training set
 train_datagen = ImageDataGenerator(rescale = 1./255,
@@ -17,49 +17,62 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
 # Creating the Training set
-training_set = train_datagen.flow_from_directory('dataset/training_set',
+training_set = train_datagen.flow_from_directory('train',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
 # Creating the Test set
-test_set = test_datagen.flow_from_directory('dataset/test_set',
+test_set = test_datagen.flow_from_directory('test',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
 
-# Part 2 - Building the CNN
+# Building the CNN
 
 # Initialising the CNN
 cnn = tf.keras.models.Sequential()
 
-# Step 1 - Convolution
+#Convolution
 cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu", input_shape=[64, 64, 3]))
 
-# Step 2 - Pooling
+ #Pooling
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='valid'))
 
 # Adding a second convolutional layer
 cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu"))
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='valid'))
 
-# Step 3 - Flattening
+# Flattening
 cnn.add(tf.keras.layers.Flatten())
 
-# Step 4 - Full Connection
+# Full Connection
 cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
 
-# Step 5 - Output Layer
+#  Output Layer
 cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 
-# Part 3 - Training the CNN
+#  Training the CNN
 
 # Compiling the CNN
 cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-# Training the CNN on the Training set and evaluating it on the Test set
+# Training the CNN on the Training set and evaluating it on the Test set(accuracy= 90%)
 cnn.fit_generator(training_set,
                   steps_per_epoch = 334,
                   epochs = 25,
                   validation_data = test_set,
                   validation_steps = 334)
+
+#For 1 image
+import numpy as np
+ 
+from keras.preprocessing import image
+ 
+test_image = image.load_img('chest_xray/test/NORMAL/IM-0009-0001.jpeg', target_size = (64, 64))                  #endpoint 1(taking user input picture)
+ 
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = cnn.predict(test_image)
+training_set.class_indices
+print(result)                                                                                                     #endpoint 2(output)
