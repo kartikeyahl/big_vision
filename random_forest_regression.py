2036a416c07de2e6585e30cd6dd3b46877a2e6dc
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-10-12 10:58:30 +0530

    Update random_forest_regression.py

--- random_forest_regression.py
+++ random_forest_regression.py
@@ -11,6 +11,12 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 l=len(X[1,:])
 
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, :])
+X[:, :] = imputer.transform(X[:, :])
+
 # Training the Random Forest Regression model on the whole dataset
 from sklearn.ensemble import RandomForestRegressor
 regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
