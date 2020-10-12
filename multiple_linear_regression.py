ed1903c88ad374ee490e3110d8eae9998f89328c
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-10-12 10:57:35 +0530

    Update multiple_linear_regression.py

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -11,6 +11,12 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 l=len(X[1,:])
 
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, :])
+X[:, :] = imputer.transform(X[:, :])
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
