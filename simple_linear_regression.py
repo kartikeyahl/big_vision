81cefb31a4f498b4e3aa2b48c69ab6f6dba1cf98
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-10-15 11:38:56 +0530

    Update simple_linear_regression.py

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -9,13 +9,14 @@ import pandas as pd
 dataset = pd.read_csv('Salary_Data.csv')                   #endpoint 1(input of .csv file)
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-l=len(X[1,:])
 
 # Taking care of missing data
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(X[:, :])
 X[:, :] = imputer.transform(X[:, :])
+
+lst = pd.read_csv('to_predict.csv')        #endpoint 2(input of .csv file for prediction)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -24,14 +25,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, rando
 # Training the Simple Linear Regression model on the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit(X_train, y_train)
-
-#taking user input for prediction
-lst=[]
-for i in range(0, l):                           
-    ele = float(input())                          #endpoint 2(taking user input values)
-    lst.append(ele)   
+regressor.fit(X_train, y_train)  
 
 # Predicting the Test set results
-y_pred = regressor.predict([lst])
-print(y_pred)                                      #endpoint 3(output)
+regressor.predict(lst)                     #endpoint 3(output)
