797617373e0c19ae78da10333d2a5c8f2182d864
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-10-15 11:45:27 +0530

    Update support_vector_regression.py

--- support_vector_regression.py
+++ support_vector_regression.py
@@ -17,6 +17,8 @@ imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(X[:, :])
 X[:, :] = imputer.transform(X[:, :])
 
+lst = pd.read_csv('to_predict.csv')    #endpoint 2(input of .csv file for prediction)
+
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
@@ -24,19 +26,11 @@ sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y)
 
-l=len(X[1,:])
-
 # Training the SVR model on the whole dataset
 from sklearn.svm import SVR
 regressor = SVR(kernel = 'rbf')
 regressor.fit(X, y)
 
-#taking user input for prediction
-lst=[]
-for i in range(0, l):                           
-    ele = float(input())                          #endpoint 2(taking user input values)
-    lst.append(ele)   
-
 # Predicting a new result
-y_pred= sc_y.inverse_transform(regressor.predict(sc_X.transform([lst])))
-print(y_pred)                                      #endpoint 3(output)
+sc_y.inverse_transform(regressor.predict(sc_X.transform(lst)))        #endpoint 3(output)
+                                    
