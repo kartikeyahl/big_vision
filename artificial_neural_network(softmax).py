db43f8d58f3df0b4a7837600cc73c8c3ce88ffcc
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-10-16 00:12:58 +0530

    Create artificial_neural_network(softmax).py

---
+++ artificial_neural_network(softmax).py
@@ -0,0 +1,60 @@
+# Artificial Neural Network
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+tf.__version__
+
+# Part 1 - Data Preprocessing
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')               #endpoint 1(input of .csv file)
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+l=len(X[1,:])
+p=(l+1)/2
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, :])
+X[:, :] = imputer.transform(X[:, :])
+
+lst = pd.read_csv('to_predict.csv')                       #endpoint 2(input of .csv file for prediction)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Part 2 - Building the ANN
+
+# Initializing the ANN
+ann = tf.keras.models.Sequential()
+
+# Adding the input layer and the first hidden layer
+ann.add(tf.keras.layers.Dense(units=p, activation='relu'))
+
+# Adding the second hidden layer
+ann.add(tf.keras.layers.Dense(units=p, activation='relu'))
+
+# Adding the output layer
+ann.add(tf.keras.layers.Dense(units=1, activation='softmax'))
+
+# Part 3 - Training the ANN
+
+# Compiling the ANN
+ann.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+# Training the ANN on the Training set
+ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
+    
+# Predicting the Test set results
+ann.predict_classes(sc.transform(lst))                             #endpoint 3(output)
+        
