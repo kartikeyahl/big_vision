b543630dbacf1db74d7cb1853550efceab9b99be
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-10-15 11:29:27 +0530

    Update polynomial_regression.py

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -9,13 +9,14 @@ import pandas as pd
 dataset = pd.read_csv('Position_Salaries.csv')             #endpoint 1(input of .csv file)
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-l=len(X[1,:])
 
 # Taking care of missing data
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(X[:, :])
 X[:, :] = imputer.transform(X[:, :])
+
+lst = pd.read_csv('to_predict.csv')    #endpoint 2(input of .csv file for prediction)
 
 # Training the Linear Regression model on the whole dataset
 from sklearn.linear_model import LinearRegression
@@ -29,12 +30,7 @@ X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
-#taking user input for prediction
-lst=[]
-for i in range(0, l):                           
-    ele = float(input())                          #endpoint 2(taking user input values)
-    lst.append(ele) 
+
 
 # Predicting a new result with Polynomial Regression
-y_pred=lin_reg_2.predict(poly_reg.fit_transform([lst]))
-print(y_pred)                                    #endpoint 3(output)
+lin_reg_2.predict(poly_reg.fit_transform(lst))               #endpoint 3(output)
