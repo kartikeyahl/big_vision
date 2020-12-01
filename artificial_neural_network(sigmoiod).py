a1456eb0ab45cb9055e6ad2f83b69e3d20ba95ff
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2020-12-01 10:18:13 +0530

    Update artificial_neural_network(sigmoiod).py

--- artificial_neural_network(sigmoiod).py
+++ artificial_neural_network(sigmoiod).py
@@ -1,19 +1,13 @@
-# Artificial Neural Network
-
-# Importing the libraries
-import numpy as np
+# Importing libraries
 import pandas as pd
-import tensorflow as tf
-tf.__version__
-
-# Part 1 - Data Preprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')               #endpoint 1(input of .csv file)
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-l=len(X[1,:])
-p=(l+1)/2
 
 # Taking care of missing data
 from sklearn.impute import SimpleImputer
@@ -23,38 +17,44 @@ X[:, :] = imputer.transform(X[:, :])
 
 lst = pd.read_csv('to_predict.csv')                       #endpoint 2(input of .csv file for prediction)
 
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder
+labelencoder_X_1 = LabelEncoder()
+y = labelencoder_X_1.fit_transform(y)
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 0)
 
-# Feature Scaling
+#Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Part 2 - Building the ANN
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
 
-# Initializing the ANN
-ann = tf.keras.models.Sequential()
+# Initialising the ANN
+ann = Sequential()
 
 # Adding the input layer and the first hidden layer
-ann.add(tf.keras.layers.Dense(units=p, activation='relu'))
+ann.add(Dense(units=16, activation='relu'))
 
 # Adding the second hidden layer
-ann.add(tf.keras.layers.Dense(units=p, activation='relu'))
+ann.add(Dense(units=16, activation='relu'))
 
 # Adding the output layer
-ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
-
-# Part 3 - Training the ANN
+ann.add(Dense(units=1, activation='sigmoid'))
 
 # Compiling the ANN
-ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# Training the ANN on the Training set
-ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
-    
+# Fitting the ANN to the Training set
+ann.fit(X_train, y_train, batch_size=100, epochs=150)
+# Long scroll ahead but worth
+# The batch size and number of epochs have been set using trial and error. Still looking for more efficient ways. Open to suggestions. 
+
 # Predicting the Test set results
-ann.predict_classes(sc.transform(lst))                             #endpoint 3(output)
-        
+ann.predict_classes(sc.transform(lst))  
